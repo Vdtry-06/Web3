9da6ab69a49bbc530edb3b54b8999948b785e822
Vdtry-06 <tunhoipro0306@gmail.com>
2025-03-15 15:34:28 +0700

    Donate to campaign

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -3,21 +3,22 @@ pragma solidity ^0.8.9;
 
 contract CrowdFunding {
     struct Campaign {
-        address owner;
-        string title;
-        string description;
-        uint256 target;
-        uint256 deadline;
-        uint256 amountCollected;
-        string image;
-        address[] donators;
-        uint256[] donations;
+        address owner; // địa chỉ người tạo chiến dịch
+        string title; // tiêu đề chiến dịch
+        string description; // mô tả chiến dịch
+        uint256 target; // số tiền (bằng wei) cần huy động để đạt mục tiêu
+        uint256 deadline; // thời gian kết thúc chiến dịch
+        uint256 amountCollected; // Tổng số tiền đã huy động được từ các nhà tài trợ
+        string image; // URL hình ảnh đại diện chiến dịch
+        address[] donators; // mảng chứa địa chỉ của những người đã khuyên góp
+        uint256[] donations; // mảng chứa số tiền tương ứng mà mỗi người đã khuyên góp
     }
 
-    mapping(uint256 => Campaign) public campaigns;
+    mapping(uint256 => Campaign) public campaigns; // lưu trữ danh sách các chiến dịch gây quỹ theo id
 
-    uint256 public numberOfCampaigns = 0;
+    uint256 public numberOfCampaigns = 0; // đếm số lượng chiến dịch đã tạo
 
+    // Tạo chiến dịch
     function createCampaign(
         address _owner, 
         string memory _title, 
@@ -32,7 +33,7 @@ contract CrowdFunding {
             // Đảm bảo deadline nằm trong tương lai
             require(campaign.deadline > block.timestamp, "The deadline should be a date in the future.");
 
-            // Gán các giá trị cho campaign
+            // Gán các giá trị cho chiến dịch
             campaign.owner = _owner;
             campaign.title = _title;
             campaign.description = _description;
@@ -48,7 +49,26 @@ contract CrowdFunding {
             return numberOfCampaigns - 1;
         }
 
-    function donateToCampaign() {}
+    // Huy động tiền cho chiến dịch
+    function donateToCampaign(uint256 _id) public payable {
+        // lấy số tiền người dung muốn khuyến góp
+        uint256 amount = msg.value;
+
+        // truy xuất chiến dịch cần nhận quyên góp
+        Campaign storage campaign = campaigns[_id];
+
+        // lưu thông tin người quyên góp
+        campaign.donators.push(msg.sender); // lưu địa chỉ
+        campaign.donations.push(amount); // lưu số tiền quyên góp
+
+        // chuyển tiền cho chủ chiến dịch
+        (bool sent, ) = payable(campaign.owner).call{value: amount}("");
+
+        // cập nhật số tiền đã thu được
+        if (sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
 
     function getDonations() {}
 
